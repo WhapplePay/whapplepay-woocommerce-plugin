025f4dbeba6c2901483e3eb628d6f1940e6792e2
NgwangShalom <ngwangshalom956@gmail.com>
2024-05-07 00:41:27 +0100

    cool

--- whapplepay-plugin.php
+++ whapplepay-plugin.php
@@ -5,7 +5,7 @@ Plugin URI: https://whapplepay.com
 Description: Integrate WhapplePay gateway with WooCommerce for secure payments and enhanced customer trust.
 Version: 1.0
 Author: Wg Organisation
-Author URI: https://whapplepay.com
+Author URI: https://wgorganisation/ngwangshalom.com
 License: GPL v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: whapplepay-plugin
